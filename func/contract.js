47f76982d4365f9686b03ab9752fefb4dbed933b
tienle <leviettien456789@gmail.com>
2024-02-03 17:57:39 +0700

    update find contract

--- func/contract.js
+++ func/contract.js
@@ -5,7 +5,8 @@ const rpcSei = "https://rpc.ankr.com/sei/a59bfb133057362ef2f650803e6cf05d9c96b78
 
 const findContract = async (ctx) => {
     try {
-        const keyWork = '"L5":"pacific-1"'
+        const keyWorkHead = 'const r=JSON.parse('
+        const keyWorkTail = `}')`
         const url = ctx.payload
         const data = await fetch(url).then(data => data.text())
         const dom = new JSDOM(data);
@@ -16,18 +17,29 @@ const findContract = async (ctx) => {
             okla.push(fetch(url + nodeList[i].src).then(data => data.text()))
         }
         const allDData = await Promise.all(okla)
-        const index = allDData[0].indexOf(keyWork) + keyWork.length
-        const [_, contractAddress] = allDData[0].slice(index, index + 69).replaceAll('"', '').split(":")
+        const indexHead = allDData[0].indexOf(keyWorkHead)
+        const indexTail = allDData[0].indexOf(keyWorkTail,indexHead)
+        const ok = allDData[0].slice(indexHead + keyWorkHead.length + 1 , indexTail +1)
+        const ecec = JSON.parse(ok)
 
-        // check CW721
         const client = await SigningCosmWasmClient.connect(rpcSei)
-        await client.queryContractSmart(contractAddress, { contract_info: {} })
+        await client.queryContractSmart(ecec['s_'], { contract_info: {} })
+
+        const info =  {
+            contract : ecec['s_'],
+            okla: ecec['Xx'].map(item=>({
+                name: item.name,
+                aa: item.allowlist ? item.allowlist.length : 'umlimited',
+            })),
+        }
         
-        return ctx.reply(contractAddress)
+        return ctx.reply(ecec['s_'])
     } catch (e) {
+        console.log("🦅 ~ e:", e)
         return ctx.reply('tim ko thay')
     }
 }
+// findContract({payload:'https://mint.seidragons.com/'})
 
 module.exports = {
     findContract: {
